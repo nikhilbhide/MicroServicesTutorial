a930b1739b5eeffd3e8ad24089217ef4bfdcd118
NikhilBhide <nikhilbhide15@gmail.com>
2018-10-12 19:23:37 +0530

    Initial Commit

---
+++ k8s-activqmq-messaging/src/test/java/com/nik/demo/messaging/k8smakemyvacation/K8sMakeMyVacationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nik.demo.messaging.k8smakemyvacation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class K8sMakeMyVacationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
