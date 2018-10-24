4f5fc2e74b6c060f09fc6a5a8a3e88ec02a0ae56
Nikhil Bhide <nikhil.bhide@fundtech.co.in>
2018-10-24 12:41:07 +0100

    added suffix to name of pod

--- k8s_Spring_Boot_HelloWorld/src/main/java/com/nik/tutorial/microservices/HelloWorld/HelloWorldController.java
+++ k8s_Spring_Boot_HelloWorld/src/main/java/com/nik/tutorial/microservices/HelloWorld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.nik.tutorial.microservices.HelloWorld;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 	@RequestMapping("/welcome")
 	private String welcome() {
+		System.out.format("\nRequest has received at time stamp %s ",LocalTime.now());
 		return "Welcome to MSA World!";
 	}
 }
