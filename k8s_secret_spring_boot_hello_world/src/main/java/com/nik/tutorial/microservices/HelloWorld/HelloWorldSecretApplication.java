fbdb93d816c60c762f53fae1890567d78cfb5ea2
Nikhil Bhide <nikhil.bhide@fundtech.co.in>
2018-12-06 15:19:23 +0000

    initial commit

---
+++ k8s_secret_spring_boot_hello_world/src/main/java/com/nik/tutorial/microservices/HelloWorld/HelloWorldSecretApplication.java
@@ -0,0 +1,12 @@
+package com.nik.tutorial.microservices.HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSecretApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSecretApplication.class, args);
+	}
+}
